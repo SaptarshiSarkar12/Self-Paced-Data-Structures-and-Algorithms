ceef4003f79271768781cdf6efbf6a319003e3bc
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2022-06-29 21:30:55 +0530

    Updated

--- src/Algebra.java
+++
@@ -1,9 +0,0 @@
-public class Algebra {
-    public static void main(String[] args) {
-        System.out.println("The difference between the functions are : " + functionDifference(2, 4));
-    }
-
-    public static double functionDifference(int x, int y){
-        return (Math.pow(x, 2) + 2 * x + 4) - ((-Math.pow(y, 2)) - (20 * y) - 1);
-    }
-}
